cfa4f10c8526e382b6d46f6d57720d66cbbba87d
Denchouka <denchouka@163.com>
2024-10-19 19:53:31 +0800

    add backend projact

---
+++ ssl-sentinel-backend/src/main/java/cool/tch/SslSentinelBackendApplication.java
@@ -0,0 +1,13 @@
+package cool.tch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslSentinelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslSentinelBackendApplication.class, args);
+    }
+
+}
