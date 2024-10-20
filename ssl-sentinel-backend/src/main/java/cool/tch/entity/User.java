fe9497967be76caa2747d3522d66e1136441fc0e
Denchouka <denchouka@163.com>
2024-10-20 22:16:51 +0800

    User表的实体类

---
+++ ssl-sentinel-backend/src/main/java/cool/tch/entity/User.java
@@ -0,0 +1,41 @@
+package cool.tch.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author denchouka
+ * @description 用户表
+ * @date 2024/10/20 21:55
+ */
+@Data
+@Builder
+@EqualsAndHashCode(callSuper = false)
+@TableName("user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 2694645802853514229L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private String secretKey;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.UPDATE)
+    private Date updateTime;
+}
