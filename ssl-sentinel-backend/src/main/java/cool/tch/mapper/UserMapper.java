e6ee8828567e90da9927694c48fcbe0595ae80bb
Denchouka <denchouka@163.com>
2024-10-24 22:04:19 +0800

    新增Mapper接口

--- ssl-sentinel-backend/src/main/java/cool/tch/mapper/UserMapper.java
+++ ssl-sentinel-backend/src/main/java/cool/tch/mapper/UserMapper.java
@@ -1,16 +1,16 @@
 package cool.tch.mapper;
 
 import cool.tch.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author denchouka
  * @description User表 Mapper接口
  * @date 2024/10/20 22:23
  */
-@Repository
+@Mapper
 public interface UserMapper {
 
-    User selectNameAndPassword(@Param("username") String username, @Param("password") String password);
+    User selectUser(@Param("username") String username);
 }
