1873ecfa089334001811f3a52d592d76886af822
Denchouka <denchouka@163.com>
2024-10-26 22:40:09 +0800

    新增一些常量

---
+++ ssl-sentinel-backend/src/main/java/cool/tch/common/Constant.java
@@ -0,0 +1,24 @@
+package cool.tch.common;
+
+/**
+ * @author denchouka
+ * @description 一些常量
+ * @date 2024/10/26 21:39
+ */
+public class Constant {
+
+    /**
+     * token密钥
+     */
+    public static final String TOKEN_SECRET_KEY = "tianchaohuaweiyueying";
+
+    /**
+     * token加密  salt
+     */
+    public static final String TOKEN_SECRET_SALT = "DENCHOUKAhelen";
+
+    /**
+     * token过期时间（1H）
+     */
+    public static final long TOKEN_EXPIRE_DATE = 60 * 60;
+}
