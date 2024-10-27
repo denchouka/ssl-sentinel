d57a7b92d13ef8b2e0d02be8d13c3961c3be003e
Denchouka <denchouka@163.com>
2024-10-27 23:16:37 +0800

    新增网络相关工具类

---
+++ ssl-sentinel-backend/src/main/java/cool/tch/util/NetUtils.java
@@ -0,0 +1,60 @@
+package cool.tch.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author denchouka
+ * @description 网络相关工具类
+ * @date 2024/10/26 22:42
+ */
+public class NetUtils {
+
+    /**
+     * 获取ip地址
+     * @param request request
+     * @return ip地址
+     */
+    public static String getIPAddress(HttpServletRequest request) {
+
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_FORWARDED");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if ("0:0:0:0:0:0:0:1".equals(ip)) {
+                // IPv6 localhost
+                ip = "127.0.0.1";
+            }
+        }
+        // 对于多层代理的情况，第一个非unknown的IP为客户端真实IP
+        if (ip != null && ip.indexOf(",") > 0) {
+            ip = ip.substring(0, ip.indexOf(",")).trim();
+        }
+        return ip;
+    }
+}
