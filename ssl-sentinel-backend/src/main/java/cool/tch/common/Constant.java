004a56c1cd7b49f599dbc29b59f15df057d1f17c
Denchouka <denchouka@163.com>
2024-10-27 23:17:02 +0800

    新增一些常量

--- ssl-sentinel-backend/src/main/java/cool/tch/common/Constant.java
+++ ssl-sentinel-backend/src/main/java/cool/tch/common/Constant.java
@@ -21,4 +21,54 @@ public class Constant {
      * token过期时间（1H）
      */
     public static final long TOKEN_EXPIRE_DATE = 60 * 60;
+
+    /**
+     * 请求的url:login
+     */
+    public static final String REQUEST_URL_LOGIN = "/login";
+
+    /**
+     * 请求头 Content-Type
+     */
+    public static final String REQUEST_HEADER_CONTENT_TYPE = "Content-Type";
+
+    /**
+     * 请求头 Access-Token
+     */
+    public static final String REQUEST_HEADER_ACCESS_TOKEN = "Access-Token";
+
+    /**
+     * 请求头 Authorization
+     */
+    public static final String REQUEST_HEADER_AUTHORIZATION = "Authorization";
+
+    /**
+     * 预检请求头
+     */
+    public static final String REQUEST_HEADER_PRE_FLIGHT = "access-control-request-headers";
+
+    /**
+     * 请求头 User-Name
+     */
+    public static final String REQUEST_HEADER_USER_NAME = "User-Name";
+
+    /**
+     * 请求源 localhost
+     */
+    public static final String REQUEST_ORIGINS_LOCALHOST = "http://localhost:5173";
+
+    /**
+     * 请求源 域名
+     */
+    public static final String REQUEST_ORIGINS_DOMAIN = "https://ssl.tch.cool";
+
+    /**
+     * 请求方法 GET
+     */
+    public static final String REQUEST_METHOD_GET = "GET";
+
+    /**
+     * 请求方法 POST
+     */
+    public static final String REQUEST_METHOD_POST = "POST";
 }
