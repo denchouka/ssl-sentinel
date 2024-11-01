f96fc8598a72c225412b85cf8082c947f301cf97
Denchouka <denchouka@163.com>
2024-11-01 23:31:49 +0800

    登录前把User-Name加入到响应头中

--- ssl-sentinel-backend/src/main/java/cool/tch/controller/UserController.java
+++ ssl-sentinel-backend/src/main/java/cool/tch/controller/UserController.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+
+import static cool.tch.common.Constant.ACCESS_CONTROL_EXPOSE_HEADERS;
+import static cool.tch.common.Constant.REQUEST_HEADER_USER_NAME;
+
 /**
  * @author denchouka
  * @description TODO
@@ -19,7 +24,10 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public ResponseResult login(@Validated @RequestBody LoginDto loginDto) {
+    public ResponseResult login(@Validated @RequestBody LoginDto loginDto, HttpServletResponse response) {
+        // 登录时请求头中没有"User-Name"，为了能在拦截器中获取到username，添加到响应头中
+        response.addHeader(ACCESS_CONTROL_EXPOSE_HEADERS,REQUEST_HEADER_USER_NAME);
+        response.setHeader(REQUEST_HEADER_USER_NAME, loginDto.getUsername());
         return userService.login(loginDto);
     }
 
