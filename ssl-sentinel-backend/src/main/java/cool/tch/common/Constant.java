5989ad274f1ff75d8bbee3f72703c4616382d4e2
Denchouka <denchouka@163.com>
2024-12-22 16:56:25 +0800

    新增一些常量

--- ssl-sentinel-backend/src/main/java/cool/tch/common/Constant.java
+++ ssl-sentinel-backend/src/main/java/cool/tch/common/Constant.java
@@ -13,6 +13,12 @@ public class Constant {
     public static final String TOKEN_SECRET_KEY = "tianchaohuaweiyueying";
 
     /**
+     * token AES加密密钥
+     * AES加密默认支持128(16字节)位密钥长度，在十六进制编码下就是32个字符
+     */
+    public static final String TOKEN_SECRET_AES_KEY = "tianchaohua1994weiyueying1997mmd";
+
+    /**
      * token加密  salt
      */
     public static final String TOKEN_SECRET_SALT = "DENCHOUKAhelen";
@@ -31,6 +37,20 @@ public class Constant {
      * 请求的url:logout
      */
     public static final String REQUEST_URL_LOGOUT = "/logout";
+
+    /**
+     * 请求的url:error
+     */
+    public static final String REQUEST_URL_ERROR = "/error";
+
+    /**
+     * 不需要校验token的请求url
+     */
+    public static final String[] REQUEST_URL_NO_VERIFY = {
+            REQUEST_URL_LOGIN,
+            REQUEST_URL_LOGOUT,
+            REQUEST_URL_ERROR
+    };
 
     /**
      * 请求头 Content-Type
