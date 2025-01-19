8c339ad4fc8e61588fb30b24b06a0e54fe8ae24f
Denchouka <denchouka@163.com>
2025-01-19 19:49:45 +0800

    日期转换出错时直接返回入参

--- ssl-sentinel-frontend/src/utils/index.js
+++ ssl-sentinel-frontend/src/utils/index.js
@@ -4,10 +4,14 @@
  * @param {*} date 
  */
 export function formatDate(date) {
-    if (date) {
-        const year = date.getFullYear()
-        const month = String(date.getMonth() + 1).padStart(2, '0')
-        const day = String(date.getDate()).padStart(2, '0')
-        return `${year}-${month}-${day}`
+    try {
+        if (date) {
+            const year = date.getFullYear()
+            const month = String(date.getMonth() + 1).padStart(2, '0')
+            const day = String(date.getDate()).padStart(2, '0')
+            return `${year}-${month}-${day}`
+        }
+    } catch {
+        return date
     }
 }
